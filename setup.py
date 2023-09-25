8a1e1cad57cc4e8a7b66bf76ef961b942254e059
Navigatoroleg <grenman@mail.ru>
2023-09-25 11:48:56 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='YooMoney_async',
     version='0.0.1',
     packages=find_packages(),
-    url='https://github.com/Navigatoroleg/yoomoney_asunc',
+    url='https://github.com/Navigatoroleg/yoomoney_asunc.git',
     author='OlesMokovoz',
     author_email='oleg-oleg-2023@mail.ru',
     description='Unofficial YooMoney_async API python library'
