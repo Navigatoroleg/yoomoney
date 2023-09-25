b2b96e5cdb9edc44fbc7948479962badd3bb4106
Navigatoroleg <grenman@mail.ru>
2023-09-25 11:58:53 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name='YooMoney_async',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     url='https://github.com/Navigatoroleg/yoomoney_asunc.git',
     author='OlesMokovoz',
